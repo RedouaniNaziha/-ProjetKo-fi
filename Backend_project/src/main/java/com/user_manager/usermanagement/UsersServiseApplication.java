ca763012f3f97615e08d045d35753cceaa1dfee1
RedouaniNaziha <naziha.redouani@etu.uae.ac.ma>
2023-01-20 01:14:18 +0100

    initial commit

---
+++ Backend_project/src/main/java/com/user_manager/usermanagement/UsersServiseApplication.java
@@ -0,0 +1,13 @@
+package com.user_manager.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.user_manager.usermanagement.UsersServiseApplication.class, args);
+	}
+
+}
